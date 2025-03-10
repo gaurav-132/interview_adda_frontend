c68477756a382d9ae4c30e9a120c4be84c1eec56
Mukul Goel <mukulgoel791@gmail.com>
2025-03-10 20:56:41 +0530

    Added signup modal component

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,12 @@
 import Image from "next/image";
 import LoginModal from "@/components/LoginModal";
+import SignupModal from "@/components/SignupModal";
 
 
 export default function Home() {
     return (
         <div className="flex justify-center items-center h-[100vh]">
-            <LoginModal />
+            <SignupModal />
         </div>
     );
 }
--- src/components/SignupModal.js
+++ src/components/SignupModal.js
@@ -4,7 +4,7 @@ import Modal from "@/components/Modal";
 import Image from "next/image";
 
 
-const LoginModal = () => {
+const SignupModal = () => {
   const [isOpen, setIsOpen] = useState(true);
   const onClose = () => setIsOpen(false);
 
@@ -27,21 +27,39 @@ const LoginModal = () => {
           <h3 className="text-xl font-bold" style={{fontSize:26}}>Start Preparation</h3>
           <p className="text-gray-500 text-sm mt-1">Please login or sign up to continue</p>
             <div className="m-4">
+                <div className="mt-4 grid grid-cols-1 md:grid-cols-2 gap-4">
+                    <input
+                        type="text"
+                        placeholder="First Name"
+                        className="w-full p-3 border rounded-lg focus:outline-none"
+                    />
+
+                    <input
+                        type="text"
+                        placeholder="Last Name"
+                        className="w-full p-3 border rounded-lg focus:outline-none"
+                    />
+                </div>
                 <input
-                type="email"
-                placeholder="Your Email"
-                className="w-full p-3 border rounded-lg mt-2 focus:outline-none"
+                    type="email"
+                    placeholder="Your Email"
+                    className="w-full p-3 border rounded-lg mt-2 focus:outline-none"
                 />
                 <input
-                type="password"
-                placeholder="Your Password"
-                className="w-full p-3 border rounded-lg mt-2 focus:outline-none"
+                    type="password"
+                    placeholder="Your Password"
+                    className="w-full p-3 border rounded-lg mt-2 focus:outline-none"
+                />
+                 <input
+                    type="password"
+                    placeholder="Confirm Your Password"
+                    className="w-full p-3 border rounded-lg mt-2 focus:outline-none"
                 />
             </div>
           
             <div  className="m-4">
                 <button className="w-full bg-[#6F63F6] text-white p-3 rounded-lg  hover:bg-[#564ccf]" style={{fontWeight:'bold',borderRadius:30}}>
-                    Login 
+                    Signup 
                 </button>
             </div>
          
@@ -53,7 +71,7 @@ const LoginModal = () => {
             </div>
 
             <p className="text-gray-500 text-sm mt-3">
-               Don't Have An Account? <a href="#" className="text-blue-500">Signup</a>
+               Already Have An Account? <a href="#" className="text-blue-500">Login</a>
             </p>
         </div>
       </div>
@@ -61,4 +79,4 @@ const LoginModal = () => {
   );
 };
 
-export default LoginModal;
+export default SignupModal;
